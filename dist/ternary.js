35ac88a012aa2021551976762824bd9a092dfc77
mizbauddintareqe <mizbauddintareq@gmail.com>
2023-05-08 22:52:30 +0600

    added assertion

--- dist/ternary.js
+++ dist/ternary.js
@@ -1,4 +1,8 @@
 "use strict";
 const isAdmin = false;
 const checkAdmin = isAdmin ? "Welcome admin" : "unauthorized";
-console.log(checkAdmin);
+// console.log(checkAdmin);
+// nullish
+const inputSomething = 3;
+const checkInput = inputSomething !== null && inputSomething !== void 0 ? inputSomething : "No input";
+console.log(checkInput);
---
+++ src/assertion.ts
@@ -0,0 +1,4 @@
+// type assertion
+
+const thinkSomething: unknown = "3444.3554452";
+console.log((thinkSomething as string).length);
