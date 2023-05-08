fbef10176bded0f49ed51767cb594c9881da6f6a
mizbauddintareqe <mizbauddintareq@gmail.com>
2023-05-08 19:32:52 +0600

    first commit

---
+++ src/array-tuples.ts
@@ -0,0 +1,12 @@
+// Array
+const friends: string[] = ["abul", "habul", "kabul"];
+const numbers: number[] = [12, 13, 14, 15];
+const mixed: (string | number | boolean)[] = ["abul", 123, false];
+
+// Tuple
+const myInfo: [string, number, boolean, object] = [
+  "mizba",
+  23,
+  false,
+  { name: "mizba", age: 23 },
+];
