b2ef2b6ce0df7bc03b4f43818ab83937008a727e
mizbauddintareqe <mizbauddintareq@gmail.com>
2023-05-09 14:59:51 +0600

    added mapped

---
+++ dist/mapped.js
@@ -0,0 +1,7 @@
+"use strict";
+const friends1 = {
+    friend1: "Mizba",
+    friend2: "abul",
+    friend3: "Babul",
+    friend4: "kabul",
+};
---
+++ src/mapped.ts
@@ -0,0 +1,17 @@
+type FriendsType = {
+  friend1: string;
+  friend2: string;
+  friend3: string;
+  friend4: string;
+};
+
+type FriendsTypeReadOnly = {
+  readonly [key in keyof FriendsType]: FriendsType[key];
+};
+
+const friends1: FriendsTypeReadOnly = {
+  friend1: "Mizba",
+  friend2: "abul",
+  friend3: "Babul",
+  friend4: "kabul",
+};
